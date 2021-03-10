1cd9b14e0918843b93ae9192991b6d1fc9871c52
Justin <justin7623@gmail.com>
2021-03-10 16:08:42 +0800

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SortingVisualiser from './sorting-visualiser/SortingVisualiser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" class="flex items-center justify-center h-screen">
+      <SortingVisualiser></SortingVisualiser>
+    </div>
+  );
+}
+
+export default App;
